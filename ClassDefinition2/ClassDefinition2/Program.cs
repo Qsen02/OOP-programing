48e3a53e687299883a76d2e91e44d4eb9200c925
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-10-19 12:15:24 +0300

    New task added

---
+++ ClassDefinition2/ClassDefinition2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassDefinition2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1......");
+            RationalNumber1 rational1=new RationalNumber1();
+            string numerator=Console.ReadLine();
+            int numeratorAsNum = int.Parse(numerator);
+            rational1.SetNumerator(numeratorAsNum);
+            string denumerator = Console.ReadLine();
+            int denumeratorAsNum=int.Parse(denumerator);
+            rational1.SetDenumerator(denumeratorAsNum);
+            RationalNumber1 rational2 = new RationalNumber1();
+            numerator = Console.ReadLine();
+            numeratorAsNum = int.Parse(numerator);
+            rational2.SetNumerator(numeratorAsNum);
+            denumerator = Console.ReadLine();
+            denumeratorAsNum = int.Parse(denumerator);
+            rational2.SetDenumerator(denumeratorAsNum);
+            RationalNumber1 rational3 = new RationalNumber1();
+            numerator = Console.ReadLine();
+            numeratorAsNum = int.Parse(numerator);
+            rational3.SetNumerator(numeratorAsNum);
+            denumerator = Console.ReadLine();
+            denumeratorAsNum = int.Parse(denumerator);
+            rational3.SetDenumerator(denumeratorAsNum);
+            Console.WriteLine("{0}/{1}", rational1.GetNumerator(), rational1.GetDenumerator());
+            Console.WriteLine("{0}/{1}", rational2.GetNumerator(), rational2.GetDenumerator());
+            Console.WriteLine("{0}/{1}", rational3.GetNumerator(), rational3.GetDenumerator());
+        }
+    }
+}
---
+++ ClassDefinition2/ClassDefinition2/RationalNumber1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDefinition2
+{
+    public class RationalNumber1
+    {
+        private int Numerator;
+        private int Denumerator;
+
+        public void SetNumerator(int num) 
+        {
+                this.Numerator = num;
+        }
+        public int GetNumerator() 
+        {
+            return this.Numerator;
+        }
+        public int GetDenumerator()
+        {
+            return this.Denumerator;
+        }
+        public void SetDenumerator(int num)
+        {
+            if (num > 0)
+            {
+                this.Denumerator = num;
+            }
+            else
+            {
+                Console.WriteLine("Denumerator must be greater than 0!");
+                return;
+            }
+        }
+    }
+}
