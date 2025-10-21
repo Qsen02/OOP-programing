2638bbc9bd9867e9a89645d4851c7ba42584bc94
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-10-21 23:28:59 +0300

    New tasks added

---
+++ TemplateClasses/TemplateClasses/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateClasses
+{
+    public class Box<T>
+    {
+        private Stack<T> Elements;
+        public int Count { get; set; }
+        public Box() 
+        {
+            this.Elements = new Stack<T>();
+            this.Count = 0;
+        }
+        public void Add(T element)
+        {
+            this.Elements.Push(element);
+            this.Count++;
+        }
+        public T Remove() 
+        {
+            T element = this.Elements.Pop();
+            this.Count--;
+            return element;
+        }
+        public void ToString() 
+        {
+            foreach (T element in this.Elements) 
+            {
+                Console.WriteLine($"{element.GetType().FullName}: {element}");
+            }
+        }
+    }
+}
---
+++ TemplateClasses/TemplateClasses/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace TemplateClasses
+{
+    internal class Program
+    {
+        static void Task1() 
+        {
+            Console.WriteLine("Task 1......");
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            Console.WriteLine(box.Remove());
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove());
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Task 2......");
+            Box<object> box = new Box<object>();
+            box.Add(123123);
+            box.Add("Life in a box");
+            box.ToString();
+        }
+        static void Task3()
+        {
+            Console.WriteLine("Task 3......");
+            Box<System.String> box = new Box<System.String>();
+            string n=Console.ReadLine();
+            int nAsNum = int.Parse(n);
+            for (int i = 0; i < nAsNum; i++) 
+            {
+                string input=Console.ReadLine();
+                box.Add(input);
+            }
+            box.ToString();
+        }
+        static void Task4()
+        {
+            Console.WriteLine("Task 4......");
+            Box<int> box = new Box<int>();
+            string n = Console.ReadLine();
+            int nAsNum = int.Parse(n);
+            for (int i = 0; i < nAsNum; i++)
+            {
+                string input = Console.ReadLine();
+                int inputAsNum=int.Parse(input);
+                box.Add(inputAsNum);
+            }
+            box.ToString();
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+        }
+    }
+}
