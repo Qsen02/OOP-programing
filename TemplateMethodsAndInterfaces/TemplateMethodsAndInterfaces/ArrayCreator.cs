f414a4e8eea96f6737d4f1fac47b3815567878f2
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-10-22 19:34:11 +0300

    New tasks added

---
+++ TemplateMethodsAndInterfaces/TemplateMethodsAndInterfaces/ArrayCreator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodsAndInterfaces
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T items)
+        {
+            T[] array=new T[length];
+            array[0] = items;
+            return array;
+        }
+        public static T[] CreateEmptyArray<T>(int length) 
+        {
+            T[] array = new T[length];
+            return array;
+        }
+        public static T[] SwapElements<T>(int firstIndex, int secondIndex, T[] array) 
+        {
+            T temp=array[firstIndex];
+            array[firstIndex]=array[secondIndex];
+            array[secondIndex]=temp;
+            return array;
+        }
+        public static void Print<T>(T[] array) 
+        {
+            foreach (T item in array)
+            {
+                Console.WriteLine($"{item.GetType().FullName}: {item}");
+            }
+        }
+        public static int ElementCounting<T>(List<T> list, T element)
+        {
+            int count = 0;
+            dynamic dyElement = element;
+            if (typeof(T) == typeof(string))
+            {
+                 count = list.Count(el => (dynamic)dyElement.Length > dyElement.Length);
+            }
+            else if(typeof(T) == typeof(double)){
+                count = list.Count(el => el > dyElement);
+            }
+            return count;
+        }
+    }
+}
---
+++ TemplateMethodsAndInterfaces/TemplateMethodsAndInterfaces/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace TemplateMethodsAndInterfaces
+{
+    internal class Program
+    {
+        static void Task1() 
+        {
+            Console.WriteLine("Task 1.....");
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Task 2.....");
+            int length=int.Parse(Console.ReadLine());
+            string[] array = ArrayCreator.CreateEmptyArray<string>(length);
+            for (int i = 0; i < length; i++) 
+            {
+                string input=Console.ReadLine();
+                array[i] = input;
+            }
+            int[] indexesToSwap=Console.ReadLine().Split(" ").Select(el=>int.Parse(el)).ToArray();
+            string[] swappedArray = ArrayCreator.SwapElements(indexesToSwap[0], indexesToSwap[1], array);
+            ArrayCreator.Print(swappedArray);
+        }
+        static void Task3() 
+        {
+            Console.WriteLine("Task 3.....");
+            int length = int.Parse(Console.ReadLine());
+            int[] array = ArrayCreator.CreateEmptyArray<int>(length);
+            for (int i = 0; i < length; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                array[i] = input;
+            }
+            int[] indexesToSwap = Console.ReadLine().Split(" ").Select(el => int.Parse(el)).ToArray();
+            int[] swappedArray = ArrayCreator.SwapElements(indexesToSwap[0], indexesToSwap[1], array);
+            ArrayCreator.Print(swappedArray);
+        }
+        static void Task4()
+        {
+            Console.WriteLine("Task 4.....");
+            int length = int.Parse(Console.ReadLine());
+            string[] array = ArrayCreator.CreateEmptyArray<string>(length);
+            for (int i = 0; i < length; i++)
+            {
+                string input = Console.ReadLine();
+                array[i] = input;
+            }
+            string elementForCount = Console.ReadLine();
+            int count = ArrayCreator.ElementCounting<string>(array.ToList(), elementForCount);
+            Console.WriteLine(count);
+        }
+        static void Task5()
+        {
+            Console.WriteLine("Task 5.....");
+            int length = int.Parse(Console.ReadLine());
+            double[] array = ArrayCreator.CreateEmptyArray<double>(length);
+            for (int i = 0; i < length; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                array[i] = input;
+            }
+            double elementForCount = double.Parse(Console.ReadLine());
+            int count = ArrayCreator.ElementCounting<double>(array.ToList(), elementForCount);
+            Console.WriteLine(count);
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            Task5();
+        }
+    }
+}
