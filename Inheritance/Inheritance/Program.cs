c7fc846b1d55d22b9cdf2137f97fcebb6a249540
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-10-24 14:32:48 +0300

    Added tasks for class inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Task1() 
+        {
+            Console.WriteLine("Task 1......");
+            Dog dog= new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Task 2......");
+            Puppy puppy= new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+        static void Task3() 
+        {
+            Console.WriteLine("Task 3......");
+            Dog dog=new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat=new Cat();
+            cat.Eat();
+            cat.Meow();
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+        }
+    }
+}
