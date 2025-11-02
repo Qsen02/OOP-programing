afba4e72b38454ec3964c488014ffd9b7258304c
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-11-02 21:20:17 +0200

    New tasks for type reflection added

---
+++ TypeReflection/TypeReflection/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Net;
+using System.Reflection;
+using System.Text;
+using TypeReflection.Task1;
+
+namespace TypeReflection
+{
+    internal class Program
+    {
+        static void Task1() 
+        {
+            Console.WriteLine("Task 1.....");
+            Spy spy=new Spy();
+            string result = spy.StealFieldsInfo("Hacker", "username", "password");
+            Console.WriteLine(result);
+            string newResult = spy.AnalyzeAcessModifiers("Hacker");
+            Console.WriteLine(newResult);
+            string privateMethods = spy.RevealPrivateMethods("Hacker");
+            Console.WriteLine(privateMethods);
+            string gettersAndSetters = spy.CollectGettersAndSetters("Hacker");
+            Console.WriteLine(gettersAndSetters);
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Task 2.....");
+            List<string> commands=new List<string>();
+            string command = "";
+            while (command != "HARVEST") 
+            {
+                command = Console.ReadLine();
+                commands.Add(command);
+            }
+            foreach (string curCommand in commands) 
+            {
+                Type classType = Type.GetType("TypeReflection.Hacker");
+                StringBuilder stringBuilder = new StringBuilder();
+                switch (curCommand) 
+                {
+                    case "public":
+                        FieldInfo[] publicFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Public);
+                        foreach (FieldInfo field in publicFields)
+                        { 
+                            stringBuilder.AppendLine($"public {field.FieldType} {field.Name}");
+                        }
+                        Console.WriteLine(stringBuilder.ToString().Trim());
+                        break;
+
+                    case "private":
+                        FieldInfo[] privateFields = classType.GetFields(BindingFlags.Instance | 
+                            BindingFlags.NonPublic)
+                            .Where((f) => f.IsPrivate).ToArray();
+                        foreach (FieldInfo field in privateFields)
+                        {
+                            stringBuilder.AppendLine($"private {field.FieldType} {field.Name}");
+                        }
+                        Console.WriteLine(stringBuilder.ToString().Trim());
+                        break;
+
+                    case "protected":
+                        FieldInfo[] protectedFields = classType.GetFields(BindingFlags.Instance | 
+                            BindingFlags.NonPublic)
+                            .Where((f)=>f.IsFamily).ToArray();
+                        foreach (FieldInfo field in protectedFields)
+                        {
+                            stringBuilder.AppendLine($"protected {field.FieldType} {field.Name}");
+                        }
+                        Console.WriteLine(stringBuilder.ToString().Trim());
+                        break;
+
+                    case "all":
+                        FieldInfo[] allFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                        foreach (FieldInfo field in allFields)
+                        {
+                            string modifier = field.IsPublic ? "public" 
+                                : field.IsPrivate?"private"
+                                : field.IsFamily ?"protected"
+                                :"";
+                            stringBuilder.AppendLine($"{modifier} {field.FieldType} {field.Name}");
+                        }
+                        Console.WriteLine(stringBuilder.ToString().Trim());
+                        break;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+        }
+    }
+}
---
+++ TypeReflection/TypeReflection/Task1/Spy.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeReflection.Task1
+{
+    public class Spy
+    {
+        public string StealFieldsInfo(string investigatedClass, params string[] requestedFields) 
+        {
+            Type classType=Type.GetType($"TypeReflection.{investigatedClass}");
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Public |
+                BindingFlags.Instance |
+                BindingFlags.NonPublic |
+                BindingFlags.Static);
+            StringBuilder stringBuilder=new StringBuilder();
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+            stringBuilder.AppendLine($"Class under investigation: {investigatedClass}");
+            foreach (FieldInfo field in classFields.Where((f) => requestedFields.Contains(f.Name))) 
+            {
+                stringBuilder.AppendLine($"{field} = {field.GetValue(classInstance)}");
+            }
+            return stringBuilder.ToString().Trim();
+        }
+        public string AnalyzeAcessModifiers(string investigatedClass) 
+        {
+            Type classType= Type.GetType($"TypeReflection.{investigatedClass}");
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Public |
+                BindingFlags.Instance |
+                BindingFlags.Static);
+            MethodInfo[] classPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo[] classNonPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            StringBuilder stringBuilder=new StringBuilder();
+
+            foreach (FieldInfo field in classFields) 
+            {
+                stringBuilder.AppendLine($"{field.Name} must be private!");
+            }
+
+            foreach (MethodInfo method in classNonPublicMethods.Where((m)=>m.Name.StartsWith("get")))
+            {
+                stringBuilder.AppendLine($"{method.Name} have to be public!");
+            }
+
+            foreach (MethodInfo method in classPublicMethods.Where((m)=>m.Name.StartsWith("set")))
+            {
+                stringBuilder.AppendLine($"{method.Name} have to be private!");
+            }
+            return stringBuilder.ToString().Trim();
+        }
+        public string RevealPrivateMethods(string investigatedClass) 
+        {
+            Type classType = Type.GetType($"TypeReflection.{investigatedClass}");
+            MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            StringBuilder stringBuilder=new StringBuilder();
+
+            stringBuilder.AppendLine($"All private methods of class: {investigatedClass}");
+            stringBuilder.AppendLine($"Base class: {classType.BaseType.Name}");
+
+            foreach (MethodInfo method in classMethods) 
+            {
+                stringBuilder.AppendLine(method.Name);
+            }
+            return stringBuilder.ToString().Trim();
+        }
+        public string CollectGettersAndSetters(string investigatedClass) 
+        {
+            Type classType = Type.GetType($"TypeReflection.{investigatedClass}");
+            MethodInfo[] methods = classType.GetMethods(BindingFlags.Instance
+                | BindingFlags.Public
+                | BindingFlags.NonPublic);
+            StringBuilder stringBuilder=new StringBuilder();
+
+            foreach (MethodInfo method in methods.Where((m)=>m.Name.StartsWith("get")))
+            {
+                stringBuilder.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (MethodInfo method in methods.Where((m) => m.Name.StartsWith("set")))
+            {
+                stringBuilder.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
