8dfc7a1c12e95fe834e3b415e3a80e1086ccd714
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-11-10 18:44:13 +0200

    New tasks added for exceptions

---
+++ CathcingExceptions/CathcingExceptions/Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CathcingExceptions
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        public Person(string firstName,string lastName,int age) 
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+        public string FirstName
+        {
+            get 
+            {
+                return this.firstName;
+            }
+            set 
+            {
+                if (string.IsNullOrEmpty(value)) 
+                {
+                    throw new ArgumentNullException(
+                        "value",
+                        "First name can't be null or empty!"
+                        );
+                }
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(
+                        "value",
+                        "Last name can't be null or empty!"
+                        );
+                }
+                this.lastName = value;
+            }
+        }
+        public int Age 
+        {
+            get
+            {
+                return this.age;
+            }
+            set 
+            {
+                if (value > 120 || value < 0) 
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "value",
+                        "Age should be in range [0...120]!"
+                        );
+                }
+                this.age = value;
+            }
+        }
+    }
+}
---
+++ CathcingExceptions/CathcingExceptions/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CathcingExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pesho = new Person("Pesho", "Peshev", 24);
+
+            try
+            {
+                Person noName = new Person(string.Empty, "Goshev", 31);
+                Person noLastName = new Person("Ivan", null, 63);
+                Person negativeAge = new Person("Stoyan", "Kolev", -1);
+                Person tooOldForThisProgram = new Person("Iskren", "Ivanov", 121);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+            catch (ArgumentOutOfRangeException ex) 
+            {
+                Console.WriteLine($"Exception thrown: {ex.Message}");
+            }
+        }
+    }
+}
