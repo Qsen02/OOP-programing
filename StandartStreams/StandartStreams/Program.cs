0ff8a4ed5b113d9aba217a2987c81e1029872ee8
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-11-12 21:51:59 +0200

    New tasks for file streams added

---
+++ StandartStreams/StandartStreams/Program.cs
@@ -0,0 +1,49 @@
+﻿using StandartStreams.Task2;
+
+namespace StandartStreams
+{
+    internal class Program
+    {
+        static void Task1() 
+        {
+            Console.WriteLine("Task 1.....");
+            try
+            {
+                List<byte> bytes = new List<byte>();
+                using (FileStream fs = new FileStream("Task1/image.jpg", FileMode.Open)) 
+                {
+                    int curByte;
+                    while ((curByte = fs.ReadByte())  != -1) 
+                    {
+                        bytes.Add((byte)curByte);
+                    }
+                }
+                using (FileStream fs = new FileStream("Task1/text.txt", FileMode.Open)) 
+                {
+                    foreach (byte curByte in bytes) 
+                    {
+                        fs.WriteByte(curByte);
+                    }
+                }
+                Console.WriteLine("Bytes copied successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("File not found!");
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void Task2() 
+        {
+            Console.WriteLine("Task 2......");
+            FileManipulator.Slice("Task1/image.jpg", "Task2", 5);
+            List<string> files = new List<string> {"part-0", "part-1", "part-2", "part-3","part-4" };
+            FileManipulator.Assemble(files, "Task2");
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+        }
+    }
+}
---
+++ StandartStreams/StandartStreams/Task2/FileManipulator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandartStreams.Task2
+{
+    public class FileManipulator
+    {
+        public static void Slice(string sourceFile,string destinationDirectory,int parts) 
+        {
+            try
+            {
+                Directory.CreateDirectory(destinationDirectory);
+                using (FileStream source = new FileStream(sourceFile, FileMode.Open))
+                {
+                    List<byte> bytes = new List<byte>();
+                    int curByte;
+                    while ((curByte = source.ReadByte()) != -1)
+                    {
+                        bytes.Add((byte)curByte);
+                    }
+                    int onePartBytes = bytes.Count / parts;
+                    int curBytes = 0;
+                    for (int i = 0; i < parts; i++)
+                    {
+                        string destinationPath = Path.Combine(destinationDirectory,$"part-{i}");
+                        using (FileStream destination = new FileStream(destinationPath, FileMode.Create,FileAccess.Write))
+                        {
+                            for (int j = curBytes; j < curBytes + onePartBytes; j++)
+                            {
+                                destination.WriteByte(bytes[j]);
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("File divided successfully!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File not found!");
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static void Assemble(List<string> files,string destinationDirectory) 
+        {
+            try
+            {
+                Directory.CreateDirectory(destinationDirectory);
+                foreach (string file in files)
+                {
+                    using (FileStream source = new FileStream($"Task2/{file}", FileMode.Open))
+                    {
+                        List<byte> bytes = new List<byte>();
+                        int curByte;
+                        while ((curByte = source.ReadByte()) != -1)
+                        {
+                            bytes.Add((byte)curByte);
+                        }
+                        string destinationPath = Path.Combine(destinationDirectory, "mergedFile");
+                        using (FileStream destination = new FileStream(destinationPath, FileMode.Append,FileAccess.Write))
+                        {
+                            foreach (byte b in bytes)
+                            {
+                                destination.WriteByte(b);
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("File merged successfully!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File not found!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
